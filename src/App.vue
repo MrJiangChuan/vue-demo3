0d84b5ac82e9b1363d4c6767ac9c0da239bce2d9
jiangchuan <jiangchuan_1994@163.com>
2020-09-14 15:50:18 +0800

    父组件向子组件传值

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,10 @@
     <h3>2、局部组件的注册与使用</h3>
     <input type="text" />
     <hello-vue></hello-vue>
+    <hr>
+    <h3>3、组件间数据传递</h3>
+    <h4>3.1 父组件--->子组件</h4>
+    <compentent2 :outer='pt'></compentent2>
   </div>
 </template>
 
@@ -15,22 +19,34 @@
   var HelloVue = {
     data() {
       return {
-        
-      } 
+
+      }
     },
     template: `
       <input type="text" />
+    `
+  }
+  var Compentent2 = {
+    props: ['outer'],
+    data() {
+      return {
+        inner: '子组件内部数据'
+      }
+    },
+    template:`
+      <p>{{inner+'---'+outer}}</p>
     `
   }
 export default {
   name: 'App',
   data() {
     return {
-     
+      pt: '父组件中内容xxx'  
     }
   },
-  comments: {
-     'hello-vue': HelloVue
+  components: {
+     'hello-vue': HelloVue,
+     'compentent2': Compentent2
   }
 }
 </script>
