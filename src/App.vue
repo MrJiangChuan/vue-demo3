92724db7939160b15474e1762a688977160447e0
jiangchuan <jiangchuan_1994@163.com>
2020-09-14 16:30:26 +0800

    子组件向父组件传值

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,10 @@
     <hr>
     <h3>3、组件间数据传递</h3>
     <h4>3.1 父组件--->子组件</h4>
-    <compentent2 :outer='pt'></compentent2>
+    <co<h4>3.1 父组件--->子组件</h4>mpentent2 :outer='pt'></compentent2>
+    <h4>3.2 子组件--->父组件</h4>
+    <div :style="{fontSize: size+'px'}">大</div>
+    <compentent3 v-on:enlarge-text='handle1'></compentent3>
   </div>
 </template>
 
@@ -37,16 +40,30 @@
       <p>{{inner+'---'+outer}}</p>
     `
   }
+  var Compentent3 = {
+    data() {
+      return {}
+    },
+    template:`<input type="button" value="子向父传递消息" @click="$emit('enlarge-text', 30)" />`
+  }
 export default {
   name: 'App',
   data() {
     return {
-      pt: '父组件中内容xxx'  
+      pt: '父组件中内容xxx',
+      size: 5
     }
   },
   components: {
      'hello-vue': HelloVue,
-     'compentent2': Compentent2
+     'compentent2': Compentent2,
+     'compentent3': Compentent3
+  },
+  methods: {
+    handle1(args) {
+      this.size += args
+      console.log(this.size)
+    }
   }
 }
 </script>
